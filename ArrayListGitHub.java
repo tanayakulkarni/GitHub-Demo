d9b9992399fc964428b90e44cdce99be83b6766e
tanayakulkarni <45399057+tanayakulkarni@users.noreply.github.com>
2018-11-27 12:39:13 -0500

    Method 1: Completed
    Added the two ArrayLists and combined it into one.

---
+++ ArrayListGitHub.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+public class ArrayListGitHub{
+	public static void main(String args[]){
+	ArrayList<Integer> list1= new ArrayList<>();
+	ArrayList<Integer> list2= new ArrayList<>();
+	for(int i=0;i<10; i+=2){
+	list1.add(i);
+	list2.add(i);
+}
+System.out.println(returnArrayList(list1, list2));
+}
+	public static ArrayList<Integer> returnArrayList(ArrayList<Integer> listone, ArrayList<Integer> listtwo){
+		ArrayList<Integer> listReturn= new ArrayList<>();
+		for(int i=0; i<listone.size(); i++){
+			listReturn.add(listone.get(i));
+	}
+	for(int i=0; i<listtwo.size(); i++){
+				listReturn.add(listtwo.get(i));
+	}
+		return listReturn;
+}
+}
